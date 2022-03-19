cf84bf2e583009f7cdede160adef1919c8672fad
liu-t-t <64963148+liu-t-t@users.noreply.github.com>
2022-03-19 18:35:16 +0800

    CMake

---
+++ Code/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/liu/Documents/Life/0.11/Life_v011
+# Build directory: /Users/liu/Documents/Life/0.11/Life_v011/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
